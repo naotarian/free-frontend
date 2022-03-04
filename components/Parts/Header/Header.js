ccb44d1c22ae6353b712265fc6bc1f601277e3a2
naotarian <naotarian19@gmail.com>
2022-03-04 06:16:14 +0000

    ログイン回り修正

--- components/Parts/Header/Header.js
+++ components/Parts/Header/Header.js
@@ -6,44 +6,52 @@ import { useCookies } from "react-cookie"
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import axios from 'axios'
+import Button from '@mui/material/Button'
+import LoginIcon from '@mui/icons-material/Login'
+import { useRouter } from 'next/router'
 const HeaderGrid = styled(Grid)`
   width: 100%;
   height: 60px;
   background: #fff;
 `
+const StyledLoginButton = styled(Button)`
+  height: 100%;
+  float: right;
+  border-radius: 0;
+`
 const Header = (props) => {
   const storeToken = useSelector((state) => state.csrf)
   const [cookies, setCookie, removeCookie] = useCookies()
+  const [authenticated, setAuthenticated] = useState(false)
+  const router = useRouter()
   let url = `${process.env.NEXT_PUBLIC_API}api/me`
-  let data = {'email': 'test@test.com', 'password': 'test' }
+  let data = {}
   let token_end_point = `${process.env.NEXT_PUBLIC_API}sanctum/csrf-cookie`
   const { token } = props;
+  //ログインチェック
   axios.post(`${process.env.NEXT_PUBLIC_API}api/me`,data, {
     headers: {
       Authorization: `Bearer ${token}`,
     }
   }).then((response) => {
+    if(response.data.data) {
+      setAuthenticated(true)
+    }
     console.log(response)
   }).catch(error => {
     const {
       status,
       statusText
     } = error.response
-  });
-    // axios.post(url,data,{ withCredentials: true }).then(res => {
-    //   console.log(res)
-    // }).catch(error => {
-    //   const {
-    //     status,
-    //     statusText
-    //   } = error.response
-    //   console.log(`Error! HTTP Status: ${status} ${statusText}`)
-    // })
+  })
+  const login = () => {
+    router.push('/auth/login')
+  }
   return (
     <HeaderGrid>
-      <Typography>
-        Header
-      </Typography>
+        {authenticated && (
+          <StyledLoginButton variant="contained" color={'primary'} startIcon={<LoginIcon />} onClick={login} type="submit">ログイン</StyledLoginButton>
+        )}
     </HeaderGrid>
   )
 }
