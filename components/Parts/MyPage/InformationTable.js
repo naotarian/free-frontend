eb3db849fcbc7e11074612195fa2891aa5c9f982
naotarian <naotarian19@gmail.com>
2022-03-10 09:00:49 +0000

    ユーザー情報更新部分

--- components/Parts/MyPage/InformationTable.js
+++ components/Parts/MyPage/InformationTable.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import styled from 'styled-components'
@@ -13,6 +13,14 @@ import Paper from '@mui/material/Paper'
 import TextField from '@mui/material/TextField'
 import LoadingButton from '@mui/lab/LoadingButton'
 import Alert from '@mui/material/Alert'
+import Typography from '@mui/material/Typography'
+import Modal from '@mui/material/Modal'
+import Box from '@mui/material/Box'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import Grid from '@mui/material/Grid'
 //icons
 import ChangeCircleIcon from '@mui/icons-material/ChangeCircle'
 const StyledButton = styled(LoadingButton)`
@@ -30,6 +38,30 @@ const StyledButton = styled(LoadingButton)`
 const EditAlert = styled(Alert)`
   margin-bottom: 2rem;
 `
+const EditLinkTypo = styled(Typography)`
+  cursor: pointer;
+  color: #2e7d32;
+`
+const StyledFormControl = styled(FormControl)`
+  margin-bottom: 2rem;
+`
+const FlexGrid = styled(Grid)`
+  display: flex;
+`
+const FlexItem = styled(Grid)`
+  margin-right: 2rem;
+`
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 const InformationTable = (props) =>  {
   const { userData } = props
   const { register, handleSubmit } = useForm()
@@ -38,7 +70,28 @@ const InformationTable = (props) =>  {
   const [loading, setLoading] = useState(false)
   const [editMessageSuccess, setEditMessageSuccess] = useState(null)
   const [editMessageNotChange, setEditMessageNotChange] = useState(null)
+  const [open, setOpen] = React.useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+  const [selectedCategoryIndex, setSelectedCategoryIndex] = useState(0)
+  const [selectedCategoryIndex2, setSelectedCategoryIndex2] = useState(0)
+  const [selectedCategory, setSelectedCategory] = useState('')
+  const [selectedCategory2, setSelectedCategory2] = useState('')
+  const [nowCategory, setNowCategory] = useState(null)
+  const [changeCategory, setChangeCategory] = useState(null)
+  const category1 = ['IT・WEB', '写真・動画', '音楽']
+  const category2 = [['WEBエンジニア', '組込系エンジニア','AIエンジニア'], ['イラストレーター', '動画編集', 'カメラマン'], ['ボイストレーナー']]
+  const nowCategoryArray = ['WEBエンジニア', '組込系エンジニア','AIエンジニア','イラストレーター', '動画編集', 'カメラマン','ボイストレーナー']
+  useEffect(() => {
+    if(userData.occupation_detail_id != null) {
+      setNowCategory(nowCategoryArray[userData.occupation_detail_id - 1])
+    }
+  }, [nowCategory])
   const onSubmit = (data) => {
+    data.category1 = selectedCategoryIndex + 1
+    data.category2 = selectedCategoryIndex2 + 1
+    data.category1Name = selectedCategory
+    data.category2Name = selectedCategory2
     //ボタンloading
     setLoading(true)
     //メールアドレス正規表現チェック
@@ -68,6 +121,8 @@ const InformationTable = (props) =>  {
       if(res.status == '200') {
         if(res.data.data.status) {
           setEditMessageSuccess(res.data.data.msg)
+          console.log(nowCategoryArray[res.data.category_detail_id - 1])
+          setChangeCategory(nowCategoryArray[res.data.category_detail_id - 1])
           setTimeout(() => {
             setEditMessageSuccess(null)
           }, 3000)
@@ -89,6 +144,30 @@ const InformationTable = (props) =>  {
       } = error.response
       console.log(`Error! HTTP Status: ${status} ${statusText}`)
     });
+  }
+  const handleChange = (event) => {
+    setSelectedCategoryIndex(event.target.value)
+    setSelectedCategory(category1[event.target.value])
+    setSelectedCategory2(category2[event.target.value][0])
+  };
+  const handleChange2 = (event) => {
+    setSelectedCategoryIndex2(event.target.value)
+    setSelectedCategory2(category2[selectedCategoryIndex][event.target.value])
+  };
+  const category = () => {
+    console.log(changeCategory)
+    console.log(nowCategory)
+    if(changeCategory) {
+      return (
+        <Typography>{changeCategory}</Typography>
+      )
+    } else if(nowCategory) {
+      return (
+        <Typography>{nowCategory}</Typography>  
+      )
+    } else {
+      <Typography>設定なし</Typography>
+    }
   }
   return (
     <React.Fragment>
@@ -136,9 +215,69 @@ const InformationTable = (props) =>  {
                 />
                 </TableCell>
               </TableRow>
+              <TableRow
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  職種
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  <FlexGrid>
+                    <FlexItem>
+                      {category()}
+                      <EditLinkTypo onClick={handleOpen}>設定を変更</EditLinkTypo>
+                    </FlexItem>
+                    <Grid>
+                      {selectedCategory &&
+                        <Typography>職種1 : {selectedCategory}を選択中</Typography>
+                      }
+                      {selectedCategory2 &&
+                        <Typography>職種2 : {selectedCategory2}を選択中</Typography>
+                      }
+                    </Grid>
+                  </FlexGrid>
+                </TableCell>
+              </TableRow>
             </TableBody>
           </Table>
           <StyledButton variant="contained" color={'primary'} startIcon={<ChangeCircleIcon />} type="submit" loading={loading}>更新</StyledButton>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <StyledFormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">職種1</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={selectedCategoryIndex}
+                label="職種1"
+                onChange={handleChange}
+              >
+                {category1.map((text, index) => (
+                  <MenuItem value={index} key={index}>{text}</MenuItem>
+                ))}
+              </Select>
+            </StyledFormControl>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">職種2</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={selectedCategoryIndex2}
+                label="職種2"
+                onChange={handleChange2}
+              >
+                {category2[selectedCategoryIndex].map((text, index) => (
+                  <MenuItem value={index} key={index}>{text}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Box>
+        </Modal>
         </form>
       </TableContainer>
     </React.Fragment>
